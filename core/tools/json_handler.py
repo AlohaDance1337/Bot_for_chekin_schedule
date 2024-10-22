3346aa32fe6aaf3041aa3b9a93739d600509603e
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2024-10-22 22:05:44 +0300

    Added asynchrony

--- core/tools/json_handler.py
+++ core/tools/json_handler.py
@@ -1,5 +1,6 @@
-import requests
-from datetime import datetime
+import aiohttp
+import asyncio
+from datetime import datetime, timedelta
 from core.tools import UniversalLogger
 
 logger = UniversalLogger('http_request')
@@ -7,7 +8,7 @@ logger = UniversalLogger('http_request')
 
 class JsonHandler:
     def __init__(self):
-        self.url = "https://sevsu.samrzhevsky.ru/api/schedule?v=3.2&section=0&institute=4&group=ИКС%2Fб-21-1-о&week=8"
+        self.base_url = "https://sevsu.samrzhevsky.ru/api/schedule?v=3.2&section=0&institute=4&group=ИКС%2Fб-21-1-о&week="
         self.numbering_of_classes = {
             1: '8:30',
             2: '10:10',
@@ -18,28 +19,37 @@ class JsonHandler:
             7: '19:00',
         }
 
-    def get_json(self):
-        try:
-            response = requests.get(self.url)
-            today = datetime.now().strftime('%d.%m.%Y')
-            if response.status_code == 200:
-                data = response.json()
-                days = data.get('schedule', [])
+    def get_current_week(self) -> int:
+        """Возвращает номер текущей недели, начиная с сентября."""
+        start_of_september = datetime(datetime.now().year, 9, 1)
+        current_week = (datetime.now() - start_of_september).days // 7 + 1
+        return current_week
 
-                # Фильтрация по завтрашней дате
-                subjects = [day for day in days if day.get('date') == today]
+    async def get_json(self):
+        """Асинхронный метод для получения данных в формате JSON."""
+        url = self.base_url + str(self.get_current_week())
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(url) as response:
+                    today = datetime.now().strftime('%d.%m.%Y')  # Завтрашняя дата
+                    if response.status == 200:
+                        data = await response.json()
+                        days = data.get('schedule', [])
 
-                sub1 = [subject for subject in subjects if subject.get('subgroup') in ('1', '0')]
-                sub2 = [subject for subject in subjects if subject.get('subgroup') in ('2', '0')]
+                        # Фильтрация по завтрашней дате
+                        subjects = [day for day in days if day.get('date') == today]
 
-                return sub1, sub2
+                        sub1 = [subject for subject in subjects if subject.get('subgroup') in ('1', '0')]
+                        sub2 = [subject for subject in subjects if subject.get('subgroup') in ('2', '0')]
 
-            logger.error(f"Failed to fetch data, status code: {response.status_code}", extra='http_request')
-            return [], []  # Возвращаем пустые списки в случае ошибки
+                        return sub1, sub2
 
-        except Exception as e:
-            logger.error(f"An error occurred: {e}", extra='http_request')
-            return [], []  # Возвращаем пустые списки в случае ошибки
+                    logger.error(f"Failed to fetch data, status code: {response.status}", extra='http_request')
+                    return [], []  # Возвращаем пустые списки в случае ошибки
+
+            except Exception as e:
+                logger.error(f"An error occurred: {e}", extra='http_request')
+                return [], []  # Возвращаем пустые списки в случае ошибки
 
     def serialize_subjects(self, subjects):
         return [
@@ -48,8 +58,9 @@ class JsonHandler:
             for subject in subjects if subject.get("n") in self.numbering_of_classes
         ]
 
-    def seralization_json(self):
-        sub1, sub2 = self.get_json()
+    async def seralization_json(self):
+        """Асинхронный метод для сериализации данных."""
+        sub1, sub2 = await self.get_json()
         str_for_sub1 = self.serialize_subjects(sub1)
         str_for_sub2 = self.serialize_subjects(sub2)
 
